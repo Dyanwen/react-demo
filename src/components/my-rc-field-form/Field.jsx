ae3a5bb21f32152adc6fa3c3101613cfcdb978dc
逗点 <dengyanwen.dyw@alibaba-inc.com>
2021-04-22 17:46:16 +0800

    feat:update

--- src/components/my-rc-field-form/Field.jsx
+++ src/components/my-rc-field-form/Field.jsx
@@ -1,12 +1,30 @@
 import React, { Component } from "react";
+import FieldContext from "./FieldContext";
 
 export default class Field extends Component {
+  static contextType = FieldContext;
+
+  getControlled = () => {
+    const { name } = this.props;
+    const { setFieldsValue, getFieldValue } = this.context;
+    return {
+      value: getFieldValue(name),
+      onChange: (event) => {
+        const newValue = event.target.value;
+        setFieldsValue({
+          [name]: newValue,
+        });
+      },
+    };
+  };
+
   render() {
     const { children, name } = this.props;
+    const returnChildNode = React.cloneElement(children, this.getControlled());
     return (
       <div>
         <label>{name}</label>
-        {children}
+        {returnChildNode}
       </div>
     );
   }
--- src/components/my-rc-field-form/FieldContext.jsx
+++ src/components/my-rc-field-form/FieldContext.jsx
@@ -2,4 +2,4 @@ import React from "react";
 
 const FieldContext = React.createContext();
 
-export default FieldContext;;
+export default FieldContext;
